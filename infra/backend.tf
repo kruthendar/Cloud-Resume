2d62396cbd1b3336230503fe2061cc262a6a9ee6
kruthendar <59464766+kruthendar@users.noreply.github.com>
2025-09-10 19:23:21 -0400

    update backend.tf terraform fmt

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cloud-resume-tfstate-kruthendar"  # match STATE_BUCKET
+    bucket         = "cloud-resume-tfstate-kruthendar"
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
